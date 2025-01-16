b3e2afc2b3e397ff3f6a730923e270d07ab58555
laniakita <101172492+laniakita@users.noreply.github.com>
2025-01-15 15:02:21 -1000

    spaces > tabs

--- src/rehype-ref-commas.ts
+++ src/rehype-ref-commas.ts
@@ -7,41 +7,41 @@ import { visit } from 'unist-util-visit';
  *
  */
 export const rehypeRefCommas = () => {
-	return (tree: Root) => {
-		visit(tree, 'element', (node) => {
-			if (node.tagName === 'p') {
-				if (node.children.length > 1) {
-					const modChildren: ElementContent[] = [];
+  return (tree: Root) => {
+    visit(tree, 'element', (node) => {
+      if (node.tagName === 'p') {
+        if (node.children.length > 1) {
+          const modChildren: ElementContent[] = [];
 
-					for (let i = node.children.length - 1; i >= 0; i--) {
-						const childNode = node.children[i];
-						const prevChildNode = node.children[i - 1];
-						modChildren.unshift(childNode as ElementContent);
+          for (let i = node.children.length - 1; i >= 0; i--) {
+            const childNode = node.children[i];
+            const prevChildNode = node.children[i - 1];
+            modChildren.unshift(childNode as ElementContent);
 
-						if (
-							childNode?.type === 'element' &&
-							childNode?.tagName === 'sup' &&
-							prevChildNode?.type === 'element' &&
-							prevChildNode?.tagName === 'sup'
-						) {
-							const supCommaSpacer = {
-								type: 'element',
-								tagName: 'sup',
-								properties: {},
-								children: [
-									{
-										type: 'text',
-										value: ', ',
-									},
-								],
-							} satisfies ElementContent;
-							modChildren.unshift(supCommaSpacer);
-						}
-					}
+            if (
+              childNode?.type === 'element' &&
+              childNode?.tagName === 'sup' &&
+              prevChildNode?.type === 'element' &&
+              prevChildNode?.tagName === 'sup'
+            ) {
+              const supCommaSpacer = {
+                type: 'element',
+                tagName: 'sup',
+                properties: {},
+                children: [
+                  {
+                    type: 'text',
+                    value: ', ',
+                  },
+                ],
+              } satisfies ElementContent;
+              modChildren.unshift(supCommaSpacer);
+            }
+          }
 
-					node.children = modChildren;
-				}
-			}
-		});
-	};
+          node.children = modChildren;
+        }
+      }
+    });
+  };
 };
