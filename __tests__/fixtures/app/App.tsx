edb3fb3d757b59c4ed71f06a66bb7ff17f4747f1
laniakita <101172492+laniakita@users.noreply.github.com>
2025-01-15 20:44:53 -1000

    working mini app fixture

--- __tests__/fixtures/app/App.tsx
+++ __tests__/fixtures/app/App.tsx
@@ -1,9 +1,32 @@
 import CommasNeeded from '../posts/commas-needed.mdx';
+import NoCommasNeeded from '../posts/no-commas-needed.mdx';
+import Nav from './components/nav';
+import './App.css';
+import { Switch, Route } from 'wouter';
+
+
 
 export default function App() {
   return (
-    <main>
-      hello
-    </main>
+    <div className='app'>
+      <Nav />
+
+      <Switch>
+        <Route path="/">Welcome to my blog!</Route>
+        <Route path="/commas-needed">
+          <main>
+            <CommasNeeded />
+          </main>
+        </Route>
+        <Route path="/no-commas-needed">
+          <main>
+            <NoCommasNeeded />
+          </main>
+        </Route>
+
+        <Route>404: Whoops! The requested page could not be found.</Route>
+      </Switch>
+
+    </div>
   );
 }
--- scripts/test-runner.ts
+++ scripts/test-runner.ts
@@ -4,23 +4,26 @@ import { mkdir } from "node:fs/promises";
 import path from "node:path";
 
 const buildTestDist = async () => {
-  const cwd = process.cwd();
-  const testsDir = './__tests__';
+  const __dirname = import.meta.dir;
+  const appBuildDir = '../__tests__/dist/app-build';
+  const appSrcDir = '../__tests__/fixtures/app';
+
   try {
     // create build output folder
-    const appBuildPath = path.join(cwd, `${testsDir}/dist/app-build`);
+    const appBuildPath = path.join(__dirname, appBuildDir);
     await mkdir(appBuildPath, {recursive: true});
     
     // copy the index.html to the folder
-    const indexHtml = Bun.file(path.join(cwd, `${testsDir}/fixtures/app/index.html`));
-    await Bun.write(path.join(cwd, `${testsDir}/dist/app-build/index.html`), indexHtml);
-    
+    const indexHtml = Bun.file(path.join(__dirname, `${appSrcDir}/index.html`));
+    await Bun.write(`${appBuildPath}/index.html`, indexHtml);
+
     // ROTFA
     await Bun.build({
-      entrypoints: [path.join(cwd, `${testsDir}/fixtures/app/index.tsx`)],
-      outdir: path.join(cwd, `${testsDir}/dist`),
+      entrypoints: [path.join(__dirname, `${appSrcDir}/index.tsx`)],
+      outdir: path.join(__dirname, appBuildDir),
       plugins: [(mdxPlugin as unknown as BunPlugin)],
       throw: true,
+      experimentalCss: true,
     });
   } catch (e) {
     const error = e as AggregateError;
