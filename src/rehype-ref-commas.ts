ff1132bd6234cf4dae8beefb800597a00dfec94c
laniakita <101172492+laniakita@users.noreply.github.com>
2025-01-15 14:32:32 -1000

    add initial implementation of rehype-ref-commas plugin with package.json and TypeScript configuration

---
+++ src/rehype-ref-commas.ts
@@ -0,0 +1,48 @@
+import type { ElementContent, Root } from 'hast';
+import { visit } from 'unist-util-visit';
+
+/**
+ * Inserts a <sup>{`, `}</sup> inbetween adjacent <sup /> elements.
+ * todo: attribute filter
+ *
+ */
+export const rehypeRefCommas = () => {
+  return (tree: Root) => {
+    visit(tree, 'element', (node) => {
+      if (node.tagName === 'p') {
+        if (node.children.length > 1) {
+          const modChildren: ElementContent[] = [];
+
+          for (let i = node.children.length - 1; i >= 0; i--) {
+            const childNode = node.children[i];
+            const prevChildNode = node.children[i - 1]; 
+            modChildren.unshift(childNode as ElementContent);
+            
+            if (
+              childNode?.type === 'element' &&
+              childNode?.tagName === 'sup' &&
+              prevChildNode?.type === 'element' &&
+              prevChildNode?.tagName === 'sup'
+            ) {
+              const supCommaSpacer = {
+                type: 'element',
+                tagName: 'sup',
+                properties: {},
+                children: [
+                  {
+                    type: 'text',
+                    value: ', ',
+                  },
+                ],
+              } satisfies ElementContent;
+              modChildren.unshift(supCommaSpacer);
+            }
+          }
+
+          node.children = modChildren;
+        }
+      }
+    });
+  };
+}
+
