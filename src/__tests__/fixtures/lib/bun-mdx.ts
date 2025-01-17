ff3635764085129b7f29e63bc3d622826e6952a0
laniakita <101172492+laniakita@users.noreply.github.com>
2025-01-16 23:31:45 -1000

    remove unused test fixtures and components, update package.json with new testing dependencies

--- src/__tests__/fixtures/lib/bun-mdx.ts
+++
@@ -1,8 +0,0 @@
-import rehypeCitationSpacer from '@/src/index';
-import mdx from '@mdx-js/esbuild';
-import remarkGfm from 'remark-gfm';
-
-export const mdxPlugin = mdx({
-  remarkPlugins: [remarkGfm],
-  rehypePlugins: [[rehypeCitationSpacer, { SuppressErr: false }]],
-});
